475726ab604085b5859952d1ce9726eeb5de46b2
ManavBhagia <manov.bhagia123@gmail.com>
2021-10-13 16:46:10 +0530

    Add files via upload

---
+++ jenkinsdemo.py
@@ -0,0 +1,10 @@
+print('My Details')
+print('------------------------------')
+print('''
+Name: Manav Bhagia
+Class/Div: TE6
+Roll No: 06
+Department: IT
+Batch: A
+''')
+print('------------------------------')
